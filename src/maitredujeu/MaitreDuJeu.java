ddf8711bc6a0afdee9a9f4c20a196f60f1f2679a
Raphaël Roche <raphael.roche@etu.unistra.fr>
2025-05-07 12:18:41 +0200

    ajout de la classe maitre du jeu

---
+++ src/maitredujeu/MaitreDuJeu.java
@@ -0,0 +1,50 @@
+package maitredujeu;
+
+public class MaitreDuJeu {
+    public MaitreDuJeu(){
+
+    }
+
+    public void creerDonjon(int longueur, int largeur){
+        String[][] carte = creerCarte(longueur, largeur);
+        positionnerObstacle();
+        creerMonstres();
+        positionnerJoueurs();
+        positionnerEquipements();
+        presenterContexte();
+    }
+
+    public String[][] creerCarte(int longueur, int largeur) {
+        String[][] carte = new String[longueur][largeur];
+        for (int i = 0; i < longueur; i++) {
+            for(int j = 0; j<largeur; j++){
+                carte[i][j] = ".";
+            }
+
+        }
+        return carte;
+    }
+
+    public void positionnerObstacle(){
+
+    }
+
+    public void creerMonstres() {
+
+
+    }
+
+    public void positionnerJoueurs(){
+
+    }
+
+    public void positionnerEquipements(){
+
+    }
+
+    public void presenterContexte(){
+
+    }
+
+
+}
