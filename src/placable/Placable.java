8c8b58bec39fc9a3f9a18216c93439efe5f40b54
Raphaël Roche <raphael.roche@etu.unistra.fr>
2025-05-16 15:13:21 +0200

    ajout du contrat de l'interface placable

--- src/placable/Placable.java
+++ src/placable/Placable.java
@@ -1,5 +1,16 @@
 package placable;
-
+/**
+ * Interface représentant un élément pouvant être placé dans le donjon.
+ * Implémentée par toutes les entités visibles sur la carte (monstres, héros, équipements, murs, etc.).
+ *
+ * Permet d'unifier le traitement des objets sur la carte :
+ * - accès à leur position (x, y),
+ * - affichage d'un nom court ou d'un symbole,
+ * - identification de leur type sans instanceof.
+ *
+ * Les méthodes par défaut (estObstacle, estEntite, etc.) permettent
+ * de savoir à quoi correspond l'objet.
+ */
 public interface Placable {
 
     void setLocation(int x, int y);
